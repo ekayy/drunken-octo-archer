cb78ba7f2ad6e981fa463223741aae52378b2164
Eric K Wu <ekaun.wu@gmail.com>
2012-05-04 17:08:00 -0700

    Finish user signup

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  attr_accessible :email, :password, :password_confirmation
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence:   true,
+                    format:     { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  
+  def self.authenticate(email, password)
+    find_by_email(email).try(:authenticate, password)
+  end
+end
